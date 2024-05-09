7dfe243b20beeebac7ff19ce86d45347d8d47481
azrieldwim <azrieldwim@gmail.com>
2024-05-09 21:57:49 +0700

    ✨ feat: Add Themes Colors

---
+++ quran_app/lib/themes/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color primary = const Color(0xFF997341);
+Color background = const Color(0xFFFDF8F2);
+Color text = const Color(0xFF584A3C);
+Color banner = const Color(0xFFFAE2C7);
+Color bottom = const Color(0xFFFBF3E9);
+Color white = const Color(0xFFFFFFFF);
+Color orange = const Color(0xFFF9B091);
