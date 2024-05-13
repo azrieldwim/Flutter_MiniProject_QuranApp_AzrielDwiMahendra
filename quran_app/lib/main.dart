970bc8977aecfe693080e52e5cd5e9d2712f20ae
azrieldwim <azrieldwim@gmail.com>
2024-05-13 16:53:34 +0700

    ✨ feat: Update main.dart

--- quran_app/lib/main.dart
+++ quran_app/lib/main.dart
@@ -3,10 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:quran_app/routes/page_route.dart';
 import 'package:quran_app/routes/route_name.dart';
 import 'package:quran_app/themes/colors.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://ocazioupoqidwxhaeotj.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jYXppb3Vwb3FpZHd4aGFlb3RqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzMTU4MjgsImV4cCI6MjAzMDg5MTgyOH0.jXIgTw5gg-yjaG38mfzhlgj4kZwUUG9rE3b3yJ0jwqk',
+  );
   runApp(const MyApp());
 }
+
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
