8bb8477c9081005a8b04346c57a47165e5ac2073
azrieldwim <azrieldwim@gmail.com>
2024-05-13 16:59:35 +0700

    ✨ feat: Update Page Route Name

--- quran_app/lib/routes/page_route.dart
+++ quran_app/lib/routes/page_route.dart
@@ -1,7 +1,11 @@
 import 'package:get/get.dart';
+import 'package:quran_app/pages/AI/views/ai_screen.dart';
+import 'package:quran_app/pages/Bookmark/views/bookmark_screen.dart';
 import 'package:quran_app/pages/Details/views/detail_screen.dart';
 import 'package:quran_app/pages/Home/views/home_screen.dart';
 import 'package:quran_app/pages/Introduction/views/introduction_screen.dart';
+import 'package:quran_app/pages/Login/views/signin_screen.dart';
+import 'package:quran_app/pages/Login/views/signup_screen.dart';
 import 'package:quran_app/routes/route_name.dart';
 
 class AppPage {
@@ -11,12 +15,28 @@ class AppPage {
       page: () => const IntroductionScreen(),
     ),
     GetPage(
+      name: RouteName.signup,
+      page: () => SignUpScreens(),
+    ),
+    GetPage(
+      name: RouteName.signin,
+      page: () => SignInScreens(),
+    ),
+    GetPage(
       name: RouteName.home,
-      page: () => const HomeScreen(),
+      page: () =>  HomeScreen(),
     ),
     GetPage(
       name: RouteName.detail,
       page: () => DetailScreen(),
+    ),
+    GetPage(
+      name: RouteName.bookmark,
+      page: () => BookmarkScreen(),
+    ),
+    GetPage(
+      name: RouteName.ai,
+      page: () => AiScreen(),
     ),
   ];
 }
--- quran_app/lib/routes/route_name.dart
+++ quran_app/lib/routes/route_name.dart
@@ -1,5 +1,9 @@
 abstract class RouteName {
   static const introduction = '/introduction';
+  static const signup = '/signup';
+  static const signin = '/signin';
   static const home = '/home';
   static const detail = '/detail';
+  static const bookmark = '/bookmark';
+  static const ai = '/ai';
 }
